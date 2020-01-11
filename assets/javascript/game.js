ff947cc52250d659bfdb9d4b7818e28c4b3d11b8
Zachary <zpyingling9559@gmail.com>
2020-01-11 07:23:06 -0500

    letters guessed can only be characters now. word gets refreshed after a loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,24 @@
 var numGuesses = 12;
 var currentWins = 0;
 var listOfGames = ["OVERWATCH", "HALO", "BATTLEFIELD"];
+var eachCharacterOfGames = [];
 var letterPresses = [];
 var currentLetter = true;
 var currentWord = "";
 var restart = false;
 var winIndicator = false;
+var blankWord = "";
+var randomNumber = 0;
 
-renderWord();
-updateWins();
+// renderWord();
+// updateWins();
 
 function renderWord() {
-  var randomNumber = Math.floor(Math.random() * 5);
+  randomNumber = Math.floor(Math.random() * 3);
+  blankWord = "";
   currentWord = listOfGames[randomNumber];
-  var blankWord = "";
   for (var i = 0; i < currentWord.length; i++) {
+    eachCharacterOfGames.push(currentWord.charAt(i));
     blankWord = blankWord + "_ ";
   }
   document.querySelector("#current-word").innerHTML = blankWord;
@@ -26,12 +30,23 @@ function updateWins() {
   currentWins++;
 }
 
-function updateGuesses(letter) {
-  // Updates number of guess and letters guessed
+function restartGame() {
+  numGuesses = 12;
+  letterPresses = [];
+  eachCharacterOfGames = [];
+  document.querySelector("#guess-number").innerHTML = numGuesses.toString();
+  document.querySelector("#letters-guessed").innerHTML = "";
+  renderWord();
+}
 
+// Updates number of guess and letters guessed
+function updateGuesses(letter) {
   // Scans through current games letter presses
   for (var i = 0; i < letterPresses.length; i++) {
-    if (letter.key.toUpperCase() === letterPresses[i]) {
+    if (
+      letter.key.toUpperCase() === letterPresses[i] ||
+      !letter.key.toUpperCase().match(/^[A-Z]+$/)
+    ) {
       // False meaning don't use current letter
       currentLetter = false;
       break;
@@ -45,17 +60,25 @@ function updateGuesses(letter) {
     document.querySelector("#guess-number").innerHTML = numGuesses.toString();
     document
       .querySelector("#letters-guessed")
-      .append(letter.key.toUpperCase() + ", ");
+      .append(letter.key.toUpperCase() + " ");
     letterPresses.push(letter.key.toUpperCase());
+
     if (numGuesses === 0) {
-      numGuesses = 12;
-      letterPresses = [];
-      document.querySelector("#guess-number").innerHTML = numGuesses.toString();
-      document.querySelector("#letters-guessed").innerHTML = "";
+      restartGame();
+    }
+  }
+}
+
+function updateWord(letter) {
+  for (var i = 0; i < eachCharacterOfGames.length; i++) {
+    if (eachCharacterOfGames[i] === letter.key) {
+    } else {
+      return;
     }
   }
 }
 
 document.onkeypress = function(event) {
   updateGuesses(event);
+  updateWord(event);
 };
